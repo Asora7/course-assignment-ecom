f9026ad95d7150afc2ae6d6e63c3052ed64034a5
Asora7 <verstr01474@stud.noroff.no>
2025-03-07 23:07:41 +0100

    style file for cart icon

--- src/components/CartIcon.jsx
+++ src/components/CartIcon.jsx
@@ -2,31 +2,7 @@
 import { useCart } from "../context/CartContext";
 import { CartLink } from "./styles/Header.styles"; 
 import { FaShoppingCart } from 'react-icons/fa';
-import styled, { keyframes, css } from "styled-components";
-
-// Bounce animation
-const bounce = keyframes`
-  0% { transform: scale(1); }
-  50% { transform: scale(1.3); }
-  100% { transform: scale(1); }
-`;
-
-const CartCount = styled.span`
-  border-radius: 50%;
-  padding: 0.25rem 0.5rem;
-  font-weight: bold;
-  margin-left: 0.5rem;
-  display: inline-block;
-  background-color: ${({ $hasItems }) => ($hasItems ? "red" : "white")};
-  color: ${({ $hasItems }) => ($hasItems ? "white" : "black")};
-  border: ${({ $hasItems }) => ($hasItems ? "none" : "1px solid black")};
-  
-  ${({ $animate }) =>
-    $animate &&
-    css`
-      animation: ${bounce} 0.5s ease;
-    `}
-`;
+import { CartCount } from './styles/CartIcon.styles';  // Importing from the new file
 
 function CartIcon() {
     const { cart, recentlyAdded } = useCart();
---
+++ src/components/styles/CartIcon.styles.js
@@ -0,0 +1,26 @@
+//components/styles/CartIcon.styles.js
+import styled, { keyframes, css } from 'styled-components';
+
+// Bounce animation
+const bounce = keyframes`
+  0% { transform: scale(1); }
+  50% { transform: scale(1.3); }
+  100% { transform: scale(1); }
+`;
+
+export const CartCount = styled.span`
+  border-radius: 50%;
+  padding: 0.25rem 0.5rem;
+  font-weight: bold;
+  margin-left: 0.5rem;
+  display: inline-block;
+  background-color: ${({ $hasItems }) => ($hasItems ? "red" : "white")};
+  color: ${({ $hasItems }) => ($hasItems ? "white" : "black")};
+  border: ${({ $hasItems }) => ($hasItems ? "none" : "1px solid black")};
+  
+  ${({ $animate }) =>
+    $animate &&
+    css`
+      animation: ${bounce} 0.5s ease;
+    `}
+`;
